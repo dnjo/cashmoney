f087d62aa58a347176c1404b59f5155c40430363
Dennis Johansson <dennis.johansson@mittmedia.se>
2017-04-22 20:46:06 +0200

    Create payments on new month

--- app/models/month.rb
+++ app/models/month.rb
@@ -3,4 +3,24 @@ class Month < ApplicationRecord
   has_many :expenses, through: :payments
 
   validates :time, presence: true
+  after_initialize :set_payments
+
+  private
+
+  def set_payments
+    return unless new_record?
+    each_active_payment { |expense| create_payment expense }
+  end
+
+  def each_active_payment
+    Expense.where(active: true).each do |expense|
+      yield expense
+    end
+  end
+
+  def create_payment(expense)
+    payment = Payment.new month: self,
+                          expense: expense
+    payments.push payment
+  end
 end
