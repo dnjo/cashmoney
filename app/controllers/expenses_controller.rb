6912df6ad01cdcb384d5046245fbe16971ff40a9
Dennis Johansson <dennis.johansson@mittmedia.se>
2017-04-23 18:34:49 +0200

    Edit expense support

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,12 +1,18 @@
 class ExpensesController < ApplicationController
   before_action :setup_expenses, only: %i[index create]
   before_action :setup_new_expense, only: :index
-  before_action :setup_start_month_options, only: %i[index create]
+  before_action :setup_start_month_options, only: %i[index create edit update]
+  before_action :setup_existing_expense, only: %i[edit update]
 
   def create
     @expense = Expense.new expense_params
     @expense.save if @expense.valid?
     render :index
+  end
+
+  def update
+    @expense.update_attributes expense_params
+    render :edit
   end
 
   private
@@ -15,6 +21,10 @@ class ExpensesController < ApplicationController
     @start_month_options = (0...12).map do |i|
       [Date::MONTHNAMES[i + 1], i]
     end
+  end
+
+  def setup_existing_expense
+    @expense = Expense.find params[:id]
   end
 
   def setup_new_expense
